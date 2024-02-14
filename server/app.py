77930b0cf91cd8d19ed1332b119d867db03618d2
hadil <hihijazi@gmail.com>
2024-02-13 22:10:14 -0500

    works without all tests passing

--- server/app.py
+++ server/app.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python3
-
-from flask import Flask, make_response, jsonify, request
+from models import db, Restaurant, RestaurantPizza, Pizza
 from flask_migrate import Migrate
+from flask import Flask, request, make_response
 from flask_restful import Api, Resource
 import os
 
-from models import db, Restaurant, RestaurantPizza, Pizza
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+DATABASE = os.environ.get("DB_URI", f"sqlite:///{os.path.join(BASE_DIR, 'app.db')}")
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.json.compact = False
 
 migrate = Migrate(app, db)
@@ -19,133 +20,78 @@ db.init_app(app)
 api = Api(app)
 
 
-@app.route('/')
+@app.route("/")
 def index():
-    return '<h1>Code challenge</h1>'
-
-@app.route('/restaurants')
-def restaurants():
-    restaurants = Restaurant.query.all()
-    response = make_response(
-        [restaurant.to_dict(rules=("-restaurant_pizzas", )) for restaurant in restaurants], 200
-    )
-
-    return response
-
-@app.route('/restaurants/<int:id>', methods=["GET"])
-def restaurant_by_id(id):
-    restaurant = Restaurant.query.filter(Restaurant.id == id).first()
-
-    if restaurant is None:
-        return make_response({"error": "Restaurant not found"}, 404)
-
-    response = make_response(restaurant.to_dict(rules=("-restaurant_pizzas",)), 200)
-
-    return response
+    return "<h1>Code challenge</h1>"
 
 
-@app.route("/pizzas", methods=["GET"])
-def pizzas():
-    pizzas = Pizza.query.all()
-    response = make_response(
-        [pizza.to_dict(rules=("-restaurant_pizzas",)) for pizza in pizzas], 200
-    )
-
-    return response
-
-
-@app.route("/pizzas/<int:id>", methods=["GET"])
-def pizzas_by_id(id):
-    pizza = Pizza.query.filter(Pizza.id == id).first()
-
-    if pizza is None:
-        return make_response({"error": "Pizza not found"}, 404)
-
-    response = make_response(pizza.to_dict(rules=("-restaurant_pizzas",)), 200)
-
-    return response
-
-@app.route("/restaurant_pizzas", methods=["POST"])
-def restaurant_pizzas():
-    try:
-        form_data = request.get_json()
-
-        if form_data is None or 'price' not in form_data:  # Check if 'price' exists in form_data
-            return make_response({"error": "Price field is missing"}, 400)
-
-        # Check if 'price' is between 1 and 30
-        price = form_data['price']
-        if not (1 <= price <= 30):
-            return make_response({"error": "Price must be between 1 and 30"}, 400)
-
-        new_restaurant_pizza = RestaurantPizza(
-            price=price,
-            restaurant_id=form_data.get("restaurant_id"),
-            pizza_id=form_data.get("pizza_id"),
-        )
-
-        db.session.add(new_restaurant_pizza)
-        db.session.commit()
-
-        response = make_response(new_restaurant_pizza.to_dict(), 201)
-    except Exception as e:
-        response = make_response({"error": str(e)}, 500)
-
-    return response
-
-
-
-
-
-
-
-
-
-@app.route("/restaurant_pizzas/<int:id>", methods=["DELETE"])
-def restaurant_pizzas_by_id(id):
-    restaurant_pizza = RestaurantPizza.query.filter(RestaurantPizza.id == id).first()
-
-    if restaurant_pizza is None:
-        return make_response({"error": "RestaurantPizza not found"}, 404)
-
-    db.session.delete(restaurant_pizza)
-    db.session.commit()
-
-    return make_response({}, 204)
-
-#API Solution 
-
-
-class RestaurantApi(Resource):
+class RestaurantClass(Resource):
     def get(self):
-        restaurants = Restaurant.query.all()
-        response = make_response(
-            [restaurant.to_dict(rules=("-restaurant_pizza",)) for restaurant in restaurants], 200
-        )
-
-        return response
-
-
-api.add_resource(RestaurantApi, "/restaurants_api")
+        # import ipdb; ipdb.set_trace()
+        restaurant_list = [
+            restaurant.to_dict(rules=("-restaurant_pizzas",))
+            for restaurant in Restaurant.query.all()
+        ]
+        return make_response(restaurant_list, 200)
 
 
-class RestaurantApiById(Resource):
+api.add_resource(RestaurantClass, "/restaurants")
+
+
+class RestaurantById(Resource):
     def get(self, id):
-        restaurant = Restaurant.query.filter(Restaurant.id == id).first()
+        res = Restaurant.query.filter_by(id=id).one_or_none()
 
-        if restaurant is None:
+        # import ipdb; ipdb.set_trace()
+        if res is not None:
+            return make_response(res.to_dict(), 200)
+        else:
             return make_response({"error": "Restaurant not found"}, 404)
 
-        response = make_response(restaurant.to_dict(), 200)
+    def delete(self, id):
+        res = Restaurant.query.filter_by(id=id).one_or_none()
 
-        return response
+        if res is None:
+            return make_response({"error": "Restaurant not found"}, 404)
+
+        db.session.delete(res)
+        db.session.commit()
+        return make_response({}, 204)
 
 
-
-api.add_resource(RestaurantApiById, "/restaurants_api/<int:id>")
-
+api.add_resource(RestaurantById, "/restaurants/<int:id>")
 
 
+class PizzaClass(Resource):
+    def get(self):
+        all_pizzas = [
+            pizza.to_dict(only=("id", "ingredients", "name"))
+            for pizza in Pizza.query.all()
+        ]
+        return make_response(all_pizzas, 200)
 
-if __name__ == '__main__':
+
+api.add_resource(PizzaClass, "/pizzas")
+
+
+class RestaurantPizzasClass(Resource):
+    def post(self):
+        # import ipdb; ipdb.set_trace()
+        try:
+            new_res_pizza = RestaurantPizza(
+                price=request.get_json()["price"],
+                pizza_id=request.get_json()["pizza_id"],
+                restaurant_id=request.get_json()["restaurant_id"],
+            )
+            db.session.add(new_res_pizza)
+            db.session.commit()
+
+            return make_response(new_res_pizza.to_dict(), 201)
+        except ValueError:
+            return make_response({"errors": ["validation errors"]}, 400)
+
+
+api.add_resource(RestaurantPizzasClass, "/restaurant_pizzas")
+
+if __name__ == "__main__":
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -4,28 +4,13 @@ from sqlalchemy.orm import validates
 from sqlalchemy.ext.associationproxy import association_proxy
 from sqlalchemy_serializer import SerializerMixin
 
-metadata = MetaData(naming_convention={
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-})
+metadata = MetaData(
+    naming_convention={
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    }
+)
 
 db = SQLAlchemy(metadata=metadata)
-
-# Add models here
-
-class Pizza(db.Model, SerializerMixin):
-    __tablename__ = "pizzas"
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    ingredients = db.Column(db.String)  
-
-    # Add relationship
-    restaurant_pizzas = db.relationship("RestaurantPizza", back_populates="pizza")
-    # Add serialization
-    serialize_rules = ("-restaurant_pizzas.pizza",)
-
-    def __repr__(self):
-        return f"<Pizza {self.id} : {self.name}, {self.ingredients}>"
 
 
 class Restaurant(db.Model, SerializerMixin):
@@ -33,15 +18,39 @@ class Restaurant(db.Model, SerializerMixin):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
-    address = db.Column(db.String)  
+    address = db.Column(db.String)
 
-    # Add relationship
-    restaurant_pizzas = db.relationship("RestaurantPizza", back_populates="restaurant")
-    # Add serialization
+    # add relationship
+    restaurant_pizzas = db.relationship(
+        "RestaurantPizza", backref="restaurant", cascade="all, delete"
+    )
+
+    # add serialization rules
     serialize_rules = ("-restaurant_pizzas.restaurant",)
 
     def __repr__(self):
-        return f"<Restaurant {self.id} : {self.name}, {self.address}>"
+        return f"<Restaurant {self.name}>"
+
+
+class Pizza(db.Model, SerializerMixin):
+    __tablename__ = "pizzas"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    ingredients = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    # add relationship
+    restaurant_pizzas = db.relationship(
+        "RestaurantPizza", backref="pizza", cascade="all, delete"
+    )
+
+    # add serialization rules
+    serialize_rules = ("-restaurant_pizzas.pizza", "-created_at", "-updated_at",)
+
+    def __repr__(self):
+        return f"<Pizza {self.name}, {self.ingredients}>"
 
 
 class RestaurantPizza(db.Model, SerializerMixin):
@@ -49,22 +58,23 @@ class RestaurantPizza(db.Model, SerializerMixin):
 
     id = db.Column(db.Integer, primary_key=True)
     price = db.Column(db.Integer, nullable=False)
-    pizza_id = db.Column(db.Integer, db.ForeignKey("pizzas.id"))
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+
+    # add relationships
     restaurant_id = db.Column(db.Integer, db.ForeignKey("restaurants.id"))
+    pizza_id = db.Column(db.Integer, db.ForeignKey("pizzas.id"))
 
-    # Add relationships
-    pizza = db.relationship("Pizza", back_populates="restaurant_pizzas")
-    restaurant = db.relationship("Restaurant", back_populates="restaurant_pizzas")
-    # Add serialization
-    serialize_rules = ("-pizza.restaurant_pizzas", "-restaurant.restaurant_pizzas")
+    # add serialization rules
+    serialize_rules = ("-restaurant_pizzas.restaurant", "-created_at", "-updated_at",)
 
-    # Add validation
+    # add validation
     @validates("price")
-    def validate_price(self, key, value):
-        if value is None or value < 0:
-            raise ValueError(f"{key} must have a value between 1 and 30")
-
-        return value
+    def validate_price(self, key, price):
+        if not 1 <= price <= 30:
+            raise ValueError("Price must be between 1 and 30")
+        return price
 
     def __repr__(self):
-        return f"<PizzaRe {self.id} : {self.price} : {self.pizza_id} : {self.restaurant_id}>"
+        return f"<RestaurantPizza ${self.price}>"
